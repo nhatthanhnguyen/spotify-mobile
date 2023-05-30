a5b33d9b3ae0487d7c151f4cb7aeff517443152b
nhontran0147 <nhontran801@gmail.com>
2023-05-31 01:17:46 +0700

    add model song history

---
+++ app/src/main/java/com/ptit/spotify/dto/model/SongHistory.java
@@ -0,0 +1,16 @@
+package com.ptit.spotify.dto.model;
+
+public class SongHistory {
+    private long song_id;
+    private String name;
+    private long album_id;
+    private long artist_id;
+    private String lyrics;
+    private long length;
+    private String url;
+    private String youtube_link;
+    private String song_cloud_id;
+    private String cover_img;
+    private String listened_time;
+}
+
---
+++ app/src/main/java/com/ptit/spotify/dto/model/SongHistoryResponse.java
@@ -0,0 +1,8 @@
+package com.ptit.spotify.dto.model;
+
+import java.util.List;
+
+public class SongHistoryResponse {
+    private List<SongHistory> song_histories;
+    private StatusError status_error;
+}
